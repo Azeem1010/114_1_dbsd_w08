7087bec0f2229969400d5ca774aad010c3daa80e
Azeem <C112181105@nkust.edu.tw>
2025-10-30 12:17:23 +0800

    3nd upload

--- ch7-1-3.php
+++ ch7-1-3.php
@@ -1,13 +1,7 @@
 <H1> SID:C112181105
 <BR>
 NAME:韓丞哲 </H1>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8" />
-<title>ch7-1-3.php</title>
-</head>
-<body>
+
 <?php 
 // 指定陣列元素
 $scores = array(22, 16, 30, 24);
